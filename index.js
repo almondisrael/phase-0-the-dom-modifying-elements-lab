908aa41cbcfdc93346ab6be3bc56f0a9b09cae0e
almondisrael <almondisrael@gmail.com>
2025-06-24 15:07:09 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+// Gets the main element by its ID and removes it
+let mainId = document.getElementById('main');
+mainId.remove();
+
+// Creates the newHeader element, sets its ID and text content, and appends it to the body
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Clive Nyaga is the champion';
+document.body.append(newHeader);
